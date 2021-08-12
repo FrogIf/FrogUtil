bc1a81d486df81b33742945f50c87b6606fe2488
frogif <skyfrog@163.com>
2021-08-12 21:55:31 +0800

    string util

---
+++ FrogUtil/Common/StringUtil.cs
@@ -0,0 +1,25 @@
+﻿namespace Frog.Util.Common
+{
+    class StringUtil
+    {
+        public static bool IsBlank(string str)
+        {
+            return str == null || "".Equals(str.Trim());
+        }
+
+        public static bool IsNotBlank(string str)
+        {
+            return str != null && !"".Equals(str.Trim());
+        }
+
+        public static bool IsEmpty(string str)
+        {
+            return str == null || "".Equals(str);
+        }
+
+        public static bool IsNotEmpty(string str)
+        {
+            return str != null && !"".Equals(str);
+        }
+    }
+}
